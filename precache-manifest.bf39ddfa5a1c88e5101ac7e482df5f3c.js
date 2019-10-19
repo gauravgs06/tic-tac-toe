763ab058558b72dad91227fb53d1ed23e19be35b
Gaurav Sharma <gauravsharmags06@gmail.com>
2019-10-19 21:48:14 +0530

    Updates

---
+++ precache-manifest.bf39ddfa5a1c88e5101ac7e482df5f3c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e9ecf453d77295ce724ed2edecf9bf3",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "afadd8429ae044f08d30",
+    "url": "/tic-tac-toe/static/css/main.8b4da9d3.chunk.css"
+  },
+  {
+    "revision": "bd8a2d45ad3d4c2cc60c",
+    "url": "/tic-tac-toe/static/js/2.2291572b.chunk.js"
+  },
+  {
+    "revision": "afadd8429ae044f08d30",
+    "url": "/tic-tac-toe/static/js/main.f6fc2b31.chunk.js"
+  },
+  {
+    "revision": "98b9abbf38060c270dd3",
+    "url": "/tic-tac-toe/static/js/runtime-main.226b0867.js"
+  }
+]);
